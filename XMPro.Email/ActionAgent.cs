e34d98ef4c48cb4a644e5e1bdfcc8c64f32c4c44
Marlie Flynn <30813912+FlynnMarlie@users.noreply.github.com>
2018-10-09 10:27:50 +1100

    Code added

---
+++ XMPro.Email/ActionAgent.cs
@@ -0,0 +1,319 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Net.Mime;
+using System.Text.RegularExpressions;
+using HtmlAgilityPack;
+using MsgReader.Outlook;
+using Newtonsoft.Json.Linq;
+using XMIoT.Framework;
+using XMIoT.Framework.Settings;
+
+namespace XMPro.Email
+{
+    public class ActionAgent : IAgent, IReceivingAgent
+    {
+        #region Global Variables
+
+        private Configuration config;
+        private List<XMIoT.Framework.Attribute> parentOutputs;
+        private SmtpClient smtp;
+        private MailMessage msg;
+        private Storage.Message msgTemplate = null;
+        private DataTable placeholdersTable = new DataTable();
+
+        private List<XMIoT.Framework.Attribute> ParentOutputs {
+            get
+            {
+                if (this.parentOutputs == null)
+                {
+                    var args = new OnRequestParentOutputAttributesArgs(this.UniqueId, "Input");
+                    this.OnRequestParentOutputAttributes.Invoke(this, args);
+                    this.parentOutputs =  args.ParentOutputs.ToList();
+                }
+                return this.parentOutputs;
+            }
+        }
+        private string SMTPServer { get { return this.config["SMTPServer"]; } }
+        private int SMTPPort
+        {
+            get
+            {
+                string port = this.config["SMTPPort"];
+                return string.IsNullOrWhiteSpace(port) ? 25 : int.Parse(port);
+            }
+        }
+        private bool UseDefaultCredentials
+        {
+            get
+            {
+                bool UseDefaultCredentials = false;
+                bool.TryParse(this.config["UseDefaultCredentials"], out UseDefaultCredentials);
+                return UseDefaultCredentials;
+            }
+        }
+        private string UserName { get { return this.config["UserName"]; } }
+        private string Password { get { return decrypt(this.config["Password"]); } }
+        private string From
+        {
+            get
+            {
+                var from = this.config["From"];
+                return from == null ? string.Empty : from.Split(";".ToCharArray())[0];
+            }
+        }
+        private string[] To
+        {
+            get
+            {
+                var to = this.config["To"];
+                return to == null ? new string[0] : to.Split(";".ToCharArray());
+            }
+        }
+        private string Subject { get { return this.config["Subject"]; } }
+
+        private string Body { get { return this.config["Body"]; } }
+
+        #endregion
+
+        #region Agent Implementation
+
+        public long UniqueId { get; set; }
+        public event EventHandler<OnPublishArgs> OnPublish;
+        public event EventHandler<OnDecryptRequestArgs> OnDecryptRequest;
+        public event EventHandler<OnRequestParentOutputAttributesArgs> OnRequestParentOutputAttributes;
+
+        private string decrypt(string value)
+        {
+            var request = new OnDecryptRequestArgs(value);
+            this.OnDecryptRequest?.Invoke(this, request);
+            return request.DecryptedValue;
+        }
+
+        public void Create(Configuration configuration)
+        {
+            this.config = configuration;
+        }
+
+        public void Destroy()
+        {
+            
+        }
+
+        public string GetConfigurationTemplate(string template, IDictionary<string, string> parameters)
+        {
+            var settingsObj = Settings.Parse(template);
+            new Populator(parameters).Populate(settingsObj);
+
+            CheckBox UseDefaultCredentials = settingsObj.Find("UseDefaultCredentials") as CheckBox;
+            TextBox UserName = settingsObj.Find("UserName") as TextBox;
+            TextBox Password = settingsObj.Find("Password") as TextBox;
+            UserName.Visible = Password.Visible = UseDefaultCredentials.Value == false;
+            TextBox Subject = settingsObj.Find("Subject") as TextBox;
+            HtmlEditor Body = settingsObj.Find("Body") as HtmlEditor;
+            Body.Visible = Subject.Visible = true;
+            DropDown From = settingsObj.Find("From") as DropDown;
+            From.Options = ParentOutputs.Select(i => new Option() { DisplayMemeber = i.Name, ValueMemeber = i.Name }).ToList();
+            DropDown To = settingsObj.Find("To") as DropDown;
+            To.Options = ParentOutputs.Select(i => new Option() { DisplayMemeber = i.Name, ValueMemeber = i.Name }).ToList();
+            var PlaceHoldersGrid = settingsObj.Find("PlaceHolders") as Grid;
+            PlaceHoldersGrid.DisableDelete = true;
+            PlaceHoldersGrid.DisableInsert = true;
+
+            if (PlaceHoldersGrid != null)
+            {
+                DropDown Mapping = PlaceHoldersGrid.Columns.First(s => s.Key == "Mapping") as DropDown;
+                Mapping.Options = this.ParentOutputs.Select(l => new Option() { DisplayMemeber = l.Name, ValueMemeber = l.Name }).ToList();
+
+                var fields = new List<String>();
+                fields = GetTemplateFields(Subject.Value, Body.Value).Keys.ToList();
+                
+                var newRows = new JArray();
+                var rows = PlaceHoldersGrid.Rows?.ToList() ?? new List<IDictionary<string, object>>();
+                foreach (var row in rows)
+                {
+                    if (fields.Contains(row["PlaceHolder"].ToString()) == true)
+                        newRows.Add(JObject.FromObject(row));
+                }
+
+                foreach (var field in fields)
+                {
+                    bool foundMatch = false;
+                    foreach (JObject row in newRows.Children<JObject>())
+                    {
+                        var d = ToDictionary(row);
+                        if (d.ContainsKey("PlaceHolder") && d["PlaceHolder"].ToString() == field)
+                        {
+                            foundMatch = true;
+                            break;
+                        }
+                    }
+
+                    if (!foundMatch)
+                    {
+                        var newRow = new JObject();
+                        newRow.Add("PlaceHolder", field);
+                        newRow.Add("Mapping", "");
+                        var idx = fields.IndexOf(field) < newRows.Count ? fields.IndexOf(field) : newRows.Count;
+                        newRows.Insert(idx, newRow);
+                    }
+                }
+                PlaceHoldersGrid.Value = newRows.ToString();
+            }
+
+            return settingsObj.ToString();
+        }
+
+        public IEnumerable<XMIoT.Framework.Attribute> GetInputAttributes(string endpoint, IDictionary<string, string> parameters)
+        {
+            return ParentOutputs;
+        }
+
+        public IEnumerable<XMIoT.Framework.Attribute> GetOutputAttributes(string endpoint, IDictionary<string, string> parameters)
+        {
+            return ParentOutputs;
+        }
+        
+        public void Start()
+        {
+            smtp = new SmtpClient();
+            smtp.SendCompleted += Smtp_SendCompleted;
+            smtp.Host = SMTPServer;
+            smtp.Port = SMTPPort;
+            smtp.EnableSsl = true;
+
+            smtp.UseDefaultCredentials = UseDefaultCredentials;
+            if (smtp.UseDefaultCredentials == false)
+                smtp.Credentials = new NetworkCredential(this.UserName, this.Password);
+        }
+
+        public void Receive(string endpointName, JArray events)
+        {
+            var grid = new Grid();
+            grid.Value = this.config["PlaceHolders"];
+
+            foreach (JObject _event in events)
+            {
+                var rtr = this.ToDictionary(_event);
+                msg = GetNewMessage(_event);
+                msg.Subject = ParseText(Subject, grid, _event);
+                msg.Body = ParseText(Body, grid, _event);
+                smtp.Send(msg);
+            }
+
+            this.OnPublish?.Invoke(this, new OnPublishArgs(events));
+        }
+
+        public string[] Validate(IDictionary<string, string> parameters)
+        {
+            List<string> errors = new List<string>();
+            this.config = new Configuration() { Parameters = parameters };
+            var PlaceHoldersGrid = new Grid();
+            PlaceHoldersGrid.Value = this.config["PlaceHoldersGrid"];
+
+            foreach (var row in PlaceHoldersGrid.Rows)
+            {
+                if (String.IsNullOrEmpty(row["Mapping"].ToString()))
+                    errors.Add($"Mapping not defined for place holder {row["PlaceHolder"].ToString()}");
+            }
+
+            return errors.ToArray();
+        }
+
+        #endregion
+
+        #region Helper methods
+
+        private MailMessage GetNewMessage(JObject record)
+        {
+            var msg = new MailMessage();
+            msg.IsBodyHtml = true;
+
+            if (From.IndexOf("@") < 0)
+                msg.From = new MailAddress(record[From].ToString());
+            else
+                msg.From = new MailAddress(From);
+
+            msg.To.Clear();
+            foreach (var to in To)
+            {
+                if (to.IndexOf("@") < 0)
+                    msg.To.Add(new MailAddress(record[to].ToString()));
+                else
+                    msg.To.Add(new MailAddress(to));
+            }
+
+            return msg;
+        }
+
+        private async void Smtp_SendCompleted(object sender, System.ComponentModel.AsyncCompletedEventArgs e)
+        {
+            if (e.Error != null)
+            {
+                //await this.Reporter.Error(this.Properties.Name, "Failed to send Email.", e.Error).ConfigureAwait(false);
+            }
+        }
+
+        private Dictionary<string, string> GetTemplateFields(String subject, String body)
+        {
+            Dictionary<string, string> fields = new Dictionary<string, string>();
+            Regex rx = new Regex(@"\{(.*?)\}");
+
+            if (!String.IsNullOrEmpty(subject))
+            {
+                MatchCollection mc = rx.Matches(subject);
+                foreach (Match match in mc)
+                {
+                    string hit = match.Value.Substring(1, match.Value.Length - 2);
+                    if (!fields.ContainsKey(hit))
+                        fields.Add(hit.Trim(), string.Empty);
+                }
+            }
+            if (!String.IsNullOrEmpty(body))
+            {
+                MatchCollection mc2 = rx.Matches(body);
+                foreach (Match match in mc2)
+                {
+                    string hit = match.Value.Substring(1, match.Value.Length - 2);
+                    if (!fields.ContainsKey(hit))
+                        fields.Add(hit.Trim(), string.Empty);
+                }
+            }
+
+            return fields;
+        }
+
+        private string ParseText(string subject, Grid grid, JObject substitutions)
+        {
+            if (grid.Rows.ToList().Count == 0)
+                return subject;
+
+            foreach (var dr in grid.Rows)
+                subject = subject.Replace("{" + dr["PlaceHolder"].ToString().Trim() + "}", substitutions[dr["Mapping"].ToString()].ToString() ?? "");
+
+            return subject;
+        }
+
+        private IDictionary<string, object> ToDictionary(JObject obj)
+        {
+            IDictionary<string, object> result = new Dictionary<string, object>();
+            PropertyDescriptorCollection properties = TypeDescriptor.GetProperties(obj);
+            foreach (PropertyDescriptor property in properties)
+            {
+                var val = property.GetValue(obj);
+                if (val != null && val is JValue)
+                    result.Add(property.Name, (val as JValue)?.Value);
+                else
+                    result.Add(property.Name, val);
+            }
+            return result;
+        }
+
+        #endregion
+    }
+}
---
+++ XMPro.Email/POP3Listener.cs
@@ -0,0 +1,237 @@
+﻿using MailKit.Net.Pop3;
+using MailKit.Security;
+using MimeKit;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using XMIoT.Framework;
+using XMIoT.Framework.Settings;
+
+namespace XMPro.Email
+{
+    public class POP3Listener : IAgent, IPollingAgent
+    {
+        private Configuration config;
+        private int mailCount;
+
+        public long UniqueId { get; set; }
+
+        public bool DisableSSLValidation
+        {
+            get
+            {
+                var temp = false;
+                bool.TryParse(this.config["DisableSSLValidation"], out temp);
+                return temp;
+            }
+        }
+
+        public string Host => this.config["Host"];
+        public int Port => int.TryParse(this.config["Port"], out var port) ? port : 995;
+        public bool UseSSL => bool.TryParse(this.config["UseSSL"], out var useSSl) ? useSSl : true;
+
+        public string Password
+        {
+            get
+            {
+                var request = new OnDecryptRequestArgs(this.config["Password"]);
+                this.OnDecryptRequest?.Invoke(this, request);
+                return request.DecryptedValue;
+            }
+        }
+
+        public string Username => this.config["Username"];
+        public bool DeleteOnRead => bool.TryParse(this.config["DeleteOnRead"], out var deleteOnRead) ? deleteOnRead : false;
+
+        public event EventHandler<OnPublishArgs> OnPublish;
+
+        public event EventHandler<OnDecryptRequestArgs> OnDecryptRequest;
+
+        public void Create(Configuration configuration)
+        {
+            this.config = configuration;
+        }
+
+        public void Destroy()
+        {
+        }
+
+        public string GetConfigurationTemplate(string template, IDictionary<string, string> parameters)
+        {
+            var settingsObj = Settings.Parse(template);
+            new Populator(parameters).Populate(settingsObj);
+
+            return settingsObj.ToString();
+        }
+
+        public IEnumerable<XMIoT.Framework.Attribute> GetOutputAttributes(string endpoint, IDictionary<string, string> parameters)
+        {
+            yield return new XMIoT.Framework.Attribute("MessageId", XMIoT.Framework.Settings.Enums.Types.String);
+            yield return new XMIoT.Framework.Attribute("ResentMessageId", XMIoT.Framework.Settings.Enums.Types.String);
+            yield return new XMIoT.Framework.Attribute("Date", XMIoT.Framework.Settings.Enums.Types.DateTime);
+            yield return new XMIoT.Framework.Attribute("ResentDate", XMIoT.Framework.Settings.Enums.Types.DateTime);
+            yield return new XMIoT.Framework.Attribute("Importance", XMIoT.Framework.Settings.Enums.Types.String);
+            yield return new XMIoT.Framework.Attribute("Priority", XMIoT.Framework.Settings.Enums.Types.String);
+            yield return new XMIoT.Framework.Attribute("Sender", XMIoT.Framework.Settings.Enums.Types.String);
+            yield return new XMIoT.Framework.Attribute("ResentSender", XMIoT.Framework.Settings.Enums.Types.String);
+            yield return new XMIoT.Framework.Attribute("From", XMIoT.Framework.Settings.Enums.Types.String);
+            yield return new XMIoT.Framework.Attribute("ResentFrom", XMIoT.Framework.Settings.Enums.Types.String);
+            yield return new XMIoT.Framework.Attribute("ReplyTo", XMIoT.Framework.Settings.Enums.Types.String);
+            yield return new XMIoT.Framework.Attribute("ResentReplyTo", XMIoT.Framework.Settings.Enums.Types.String);
+            yield return new XMIoT.Framework.Attribute("To", XMIoT.Framework.Settings.Enums.Types.String);
+            yield return new XMIoT.Framework.Attribute("Cc", XMIoT.Framework.Settings.Enums.Types.String);
+            yield return new XMIoT.Framework.Attribute("ResentCc", XMIoT.Framework.Settings.Enums.Types.String);
+            yield return new XMIoT.Framework.Attribute("Bcc", XMIoT.Framework.Settings.Enums.Types.String);
+            yield return new XMIoT.Framework.Attribute("ResentBcc", XMIoT.Framework.Settings.Enums.Types.String);
+            yield return new XMIoT.Framework.Attribute("InReplyTo", XMIoT.Framework.Settings.Enums.Types.String);
+            yield return new XMIoT.Framework.Attribute("Subject", XMIoT.Framework.Settings.Enums.Types.String);
+            yield return new XMIoT.Framework.Attribute("TextBody", XMIoT.Framework.Settings.Enums.Types.String);
+            yield return new XMIoT.Framework.Attribute("HtmlBody", XMIoT.Framework.Settings.Enums.Types.String);
+            yield return new XMIoT.Framework.Attribute("Attachments", XMIoT.Framework.Settings.Enums.Types.String);
+        }
+
+        public void Poll()
+        {
+            using (var client = new Pop3Client())
+            {
+                this.initialize(client);
+                var deleted = 0;
+                for (; this.mailCount < client.Count; this.mailCount++)
+                {
+                    try
+                    {
+                        var message = client.GetMessage(this.mailCount);
+                        this.OnPublish?.Invoke(this, new OnPublishArgs(this.composeOutput(message)));
+                        if (this.DeleteOnRead)
+                        {
+                            client.DeleteMessage(this.mailCount);
+                            deleted++;
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+#warning This should be logged properly.
+                        Console.WriteLine($"{DateTime.UtcNow}|ERROR|{nameof(POP3Listener)}|{ex.ToString()}");
+                    }
+                }
+
+                client.Disconnect(true);
+                //sucessfully deleted
+                if (this.DeleteOnRead)
+                    this.mailCount -= deleted;
+            }
+        }
+
+        public void Start()
+        {
+            using (var client = new Pop3Client())
+            {
+                this.initialize(client);
+
+                this.mailCount = client.Count;
+
+                client.Disconnect(true);
+            }
+        }
+
+        public string[] Validate(IDictionary<string, string> parameters)
+        {
+            int i = 1;
+            var errors = new List<string>();
+            this.config = new Configuration() { Parameters = parameters };
+
+            if (String.IsNullOrWhiteSpace(this.Host))
+                errors.Add($"Error {i++}: Host address is not specified.");
+
+            if (String.IsNullOrWhiteSpace(this.Username))
+                errors.Add($"Error {i++}: Username is not specified.");
+
+            return errors.ToArray();
+        }
+
+        private void initialize(Pop3Client client)
+        {
+            if (this.DisableSSLValidation)
+                client.ServerCertificateValidationCallback = (s, c, h, e) => true;
+
+            client.Connect(this.Host, this.Port, this.UseSSL);
+
+            client.Authenticate(this.Username, this.Password);
+        }
+
+        private Array composeOutput(params MimeMessage[] messages)
+        {
+            var events = new List<IDictionary<string, object>>();
+            foreach (var message in messages)
+            {
+                var rtr = new Dictionary<string, object>();
+                rtr.Add("MessageId", message.MessageId);
+                rtr.Add("ResentMessageId", message.ResentMessageId);
+                rtr.Add("Date", message.Date.DateTime);
+                rtr.Add("ResentDate", message.ResentDate.DateTime);
+                rtr.Add("Importance", message.Importance.ToString());
+                rtr.Add("Priority", message.Priority.ToString());
+                rtr.Add("Sender", message.Sender?.ToString());
+                rtr.Add("ResentSender", message.ResentSender?.ToString());
+                rtr.Add("From", String.Join(";", message.From));
+                rtr.Add("ResentFrom", String.Join(";", message.ResentFrom));
+                rtr.Add("ReplyTo", String.Join(";", message.ReplyTo));
+                rtr.Add("ResentReplyTo", String.Join(";", message.ResentReplyTo));
+                rtr.Add("To", String.Join(";", message.To));
+                rtr.Add("Cc", String.Join(";", message.Cc));
+                rtr.Add("ResentCc", String.Join(";", message.ResentCc));
+                rtr.Add("Bcc", String.Join(";", message.Bcc));
+                rtr.Add("ResentBcc", String.Join(";", message.ResentBcc));
+                rtr.Add("InReplyTo", message.InReplyTo);
+                rtr.Add("Subject", message.Subject);
+                rtr.Add("TextBody", message.TextBody);
+                rtr.Add("HtmlBody", message.HtmlBody);
+
+                var attachments = new List<string>();
+                foreach (MimeEntity attachment in message.Attachments)
+                {
+                    var fileName = attachment.ContentDisposition?.FileName ?? attachment.ContentType.Name;
+                    var dirPath = this.getRandomDirName();
+                    Directory.CreateDirectory(dirPath);
+                    var path = Path.Combine(dirPath, fileName);
+                    using (var stream = System.IO.File.Create(Path.Combine(dirPath, fileName)))
+                    {
+                        if (attachment is MessagePart)
+                        {
+                            var rfc822 = (MessagePart)attachment;
+                            rfc822.Message.WriteTo(stream);
+                        }
+                        else
+                        {
+                            var part = (MimePart)attachment;
+                            part.Content.DecodeTo(stream);
+                        }
+                    }
+                    attachments.Add(path);
+                }
+                rtr.Add("Attachments", String.Join("|", attachments));
+
+                events.Add(rtr);
+            }
+            return events.ToArray();
+        }
+
+        private string getRandomDirName()
+        {
+            var tempDir = System.IO.Path.GetTempPath();
+
+            string fullPath;
+            do
+            {
+                var randomName = System.IO.Path.GetRandomFileName();
+                fullPath = System.IO.Path.Combine(tempDir, randomName);
+            }
+            while (Directory.Exists(fullPath));
+
+            return fullPath;
+        }
+    }
+}
